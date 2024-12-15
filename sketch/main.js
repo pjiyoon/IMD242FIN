39eb8493bf1e9efc4cb974d830cde8c9289ef353
adhusio <164802067+pjiyoon@users.noreply.github.com>
2024-12-16 08:43:11 +0900

    Final

--- sketch/main.js
+++ sketch/main.js
@@ -1,7 +1,7 @@
 // 종횡비를 고정하고 싶을 경우: 아래 두 변수를 0이 아닌 원하는 종, 횡 비율값으로 설정.
 // 종횡비를 고정하고 싶지 않을 경우: 아래 두 변수 중 어느 하나라도 0으로 설정.
-const aspectW = 4;
-const aspectH = 3;
+const aspectW = 0;
+const aspectH = 0;
 // html에서 클래스명이 container-canvas인 첫 엘리먼트: 컨테이너 가져오기.
 const container = document.body.querySelector('.container-canvas');
 // 필요에 따라 이하에 변수 생성.
